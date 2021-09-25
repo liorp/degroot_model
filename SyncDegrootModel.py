4c2da724c8fb8c304cdc0f8136cfc960bc9fbbc4
Lior Pollak <4294489+liorp@users.noreply.github.com>
2021-09-25 18:28:29 +0300

    Better graphing

--- SyncDegrootModel.py
+++ SyncDegrootModel.py
@@ -10,17 +10,24 @@ class SyncDegrootModel:
     
     def __init__(self, nodes=100, edge_probability=0.5, P=2, initial_opinions_distribution=np.random.uniform) -> None:
         self._graph = nx.fast_gnp_random_graph(nodes, edge_probability)
-        self._energy = lambda A: lambda x: reduce(lambda s, A_i: s + np.abs(x - A_i)**P, A, 0) # A is the neighbors' opinions
+        self._energy = lambda A: lambda x: reduce(lambda s, A_i: np.add(s, np.float_power(np.abs(x - A_i), P)), A, 0) # A is the neighbors' opinions
         self._init_opinions(initial_opinions_distribution)
 
     def _init_opinions(self, distribution: Callable[[], np.double]):
         for n in self._graph.nodes:
             self._graph.nodes[n]["opinion"] = distribution()
+        for n in self._graph.nodes:
+            self._graph.nodes[n]["energy"] = self._energy([self._graph.nodes[i]["opinion"] for i in self._graph.adj[n]])(self._graph.nodes[n]["opinion"])
 
     def one_step_degroot_update(self):
         for n in self._graph.nodes:
             result = optimize.minimize_scalar(self._energy([self._graph.nodes[i]["opinion"] for i in self._graph.adj[n]]))
             x_min = result.x
             self._graph.nodes[n]["next_opinion"] = x_min
+            self._graph.nodes[n]["energy"] = self._energy([self._graph.nodes[i]["opinion"] for i in self._graph.adj[n]])(x_min)
         for n in self._graph.nodes:
-            self._graph.nodes[n]["next_opinion"] = self._graph.nodes[n]["opinion"]
+            self._graph.nodes[n]["opinion"] = self._graph.nodes[n]["next_opinion"]
+
+    def get_graph_energy(self) -> float:
+        labels = nx.get_node_attributes(self._graph, 'energy')
+        return sum([labels[l] for l in labels])
--- minimize_lp_energy.py
+++ minimize_lp_energy.py
@@ -5,13 +5,39 @@ from matplotlib import pyplot as plt
 import networkx as nx
 from SyncDegrootModel import SyncDegrootModel
 
+ITERATIONS = 10
+
 def main():
-    degroot = SyncDegrootModel(nodes=10, edge_probability=0.8)
-    for i in range(1000):
+    degroot = SyncDegrootModel(nodes=30, edge_probability=1, P=20, initial_opinions_distribution=lambda: np.random.uniform(5, 50))
+    energies = []
+    energy = degroot.get_graph_energy()
+    energies.append(energy)
+    print(f"Initial Energy: f{energy}")
+
+    for i in range(ITERATIONS):
         degroot.one_step_degroot_update()
-    labels = nx.get_node_attributes(degroot._graph, 'opinion')
-    nx.draw_networkx(degroot._graph, labels=labels)
+        energy = degroot.get_graph_energy()
+        energies.append(energy)
+        print(f"{i+1} Energy: f{energy}")
+
+    # plt.figure("Energy")
+    # labels = nx.get_node_attributes(degroot._graph, "energy")
+    # nx.draw_networkx(degroot._graph, labels=labels)
+
+    # plt.figure("Opinion")
+    # labels = nx.get_node_attributes(degroot._graph, "opinion")
+    # nx.draw_networkx(degroot._graph, labels=labels)
+
+    plt.figure("Total Energy (log) vs Time")
+    plt.scatter(range(ITERATIONS + 1), np.log10(energies))
+    m, b = np.polyfit(range(ITERATIONS + 1), np.log10(energies), 1)
+    plt.plot(range(ITERATIONS + 1), m*range(ITERATIONS + 1) + b, "r--", label=f"y={m:.2f}x+{b:.2f}")
+    plt.legend(fontsize=9)
+    plt.xlabel("Time")
+    plt.ylabel("Energy")
+    
     plt.show()
+
 
 if __name__ == "__main__":
     main()
